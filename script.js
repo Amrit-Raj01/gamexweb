7d5cdbea8586ddf48ec8783c501ac8ed949aa540
Amrit-Raj01 <amritrajsingh951@gmail.com>
2025-02-15 18:13:45 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,69 @@
+// Open Category Page Function
+function openCategory(category) {
+    window.location.href = category + ".html"; // Redirect to category page
+}
+
+// Particle Background Effect
+const canvas = document.getElementById('particles');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const particlesArray = [];
+const numParticles = 100;
+
+// Particle Class
+class Particle {
+    constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 5 + 1;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+        this.color = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    }
+
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        if (this.x < 0 || this.x > canvas.width) this.speedX *= -1;
+        if (this.y < 0 || this.y > canvas.height) this.speedY *= -1;
+    }
+
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.closePath();
+        ctx.fill();
+    }
+}
+
+// Initialize Particles
+function initParticles() {
+    for (let i = 0; i < numParticles; i++) {
+        particlesArray.push(new Particle());
+    }
+}
+
+// Animate Particles
+function animateParticles() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    particlesArray.forEach(particle => {
+        particle.update();
+        particle.draw();
+    });
+    requestAnimationFrame(animateParticles);
+}
+
+// Resize Event
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+
+// Start Animation
+initParticles();
+animateParticles();
